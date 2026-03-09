49dfe31e8817469afef0c54ff80de56514c756af
odongotim <timothyodongo263@gmail.com>
2026-03-09 15:47:53 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'arem-medicare-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo.jpg',
+  './manifest.json'
+];
+
+// Install: Save the files to the phone's memory
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch: If offline, grab files from the cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
